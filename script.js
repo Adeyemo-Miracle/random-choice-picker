39a5409599686d4d1cbd0863b55e03a2e8a377e8
Adeyemo-Miracle <adeyemomoyin@gmail.com>
2024-01-14 20:32:28 +0100

    add script

--- script.js
+++ script.js
@@ -4,9 +4,29 @@ const textarea = document.getElementById('textarea')
 textarea.focus()
 textarea.addEventListener('keyup', (e)=>{
     createTags(e.currentTarget.value)
+    if(e.key === 'Enter'){
+        setTimeout(()=>{
+            e.target.value=''
+        })
+        randomSelect()
+    }
 })
 function createTags(input){
     const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag=> tag.trim())
     tagsEl.innerHTML=''
-    tags.forEach()
+    tags.forEach(tag =>{
+        const tagEl = document.createElement('span')
+        tagEl.classList.add('tag')
+        tagEl.innerText = tag
+        tagsEl.appendChild(tagsEl)
+    })
+}
+const randomSelect = ()=>{
+    const times =30
+    const interval = setInterval(()=>{
+        const randomTag = pickRandomTag()
+    }, 100)
+}
+function pickRandomTag(){
+    const tags = document.querySelectorAll('.tags')
 }
